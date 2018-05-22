03e7752df3d0bc0f6431fc454f97888abf17129b
iKenshu <undefined.hlo.o@gmail.com>
2018-05-22 11:07:56 -0400

    more ajax stuff

--- tweets/forms.py
+++ tweets/forms.py
@@ -4,25 +4,19 @@ from .models import Tweet
 
 
 class TweetModelForm(forms.ModelForm):
-    # content = forms.CharField(
-    #     label='',
-    #     widget=forms.Textarea(
-    #         attrs={
-    #             'placeholder': "Your message",
-    #             'class': "form-control",
-    #             'rows': 5,
-    #             'cols': 100,
-    #         })
-    # )
+    content = forms.CharField(
+        label='',
+        widget=forms.Textarea(
+            attrs={
+                'placeholder': "Your message",
+                'class': "form-control",
+                'rows': 5,
+                'cols': 100,
+            })
+    )
 
     class Meta:
         model = Tweet
         fields = [
             "content",
         ]
-
-    # def clean_content(self, *args, **kwargs):
-    #     content = self.cleaned_data.get("content")
-    #     if content == "abc":
-    #         raise forms.ValidationError("Cannont be ABC")
-    #     return content
